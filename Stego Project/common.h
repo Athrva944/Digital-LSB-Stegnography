59e43f19724c5c9a982ebe18a3a027305d0bd164
Atharva944 <160106933+Athrva944@users.noreply.github.com>
2024-05-26 11:26:14 +0530

    Add files via upload

---
+++ Stego Project/common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/* Magic string to identify whether stegged or not */
+#define MAGIC_STRING "#*"
+#define MAX_EXT_SIZE 10
+#define MAX_FILE_NAME 10
+#define MAX_FILE_SIZE 100
+#endif
