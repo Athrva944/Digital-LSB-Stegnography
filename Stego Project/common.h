723d3937c1f01de54d60a0f83999777c7d5c9cd5
Atharva944 <160106933+Athrva944@users.noreply.github.com>
2024-05-26 11:57:00 +0530

    Delete Stego Project directory

--- Stego Project/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-/* Magic string to identify whether stegged or not */
-#define MAGIC_STRING "#*"
-#define MAX_EXT_SIZE 10
-#define MAX_FILE_NAME 10
-#define MAX_FILE_SIZE 100
-#endif
